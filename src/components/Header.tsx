577ceb300c6eb339e3926e72903793fbd7edf46d
Ahmed Alexander <92260103+aalxer@users.noreply.github.com>
2024-05-07 15:03:00 +0200

    create header component with css-stylesheet

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+// musste extra in declaration.d.ts deklariert werden, sonst sind nur module zulässig zu importieren
+import Logo from "../images/bookspot-logo.png";
+import "./Header.css"
+
+export default function header() {
+   return <header className="main-header-container">
+       <Navbar></Navbar>
+       <img src={Logo} alt="logo"/>
+   </header>
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function navbar() {
+    return <div className="navbar-container">
+        <ul className="navbar-container-ul">
+            <li>Contact</li>
+            <li>Cart</li>
+        </ul>
+    </div>
+}
