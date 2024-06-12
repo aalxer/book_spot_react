79e43e5f1cee1edb83e32ba0c8333e36d6cf75f5
Ahmed Alexander <92260103+aalxer@users.noreply.github.com>
2024-06-12 12:39:14 +0200

    show error message in ErrorScreen

--- src/components/body/ErrorScreen.tsx
+++ src/components/body/ErrorScreen.tsx
@@ -1,10 +1,17 @@
 import ErrorIcon from '../../images/error-icon-red.png'
 import './ErrorScreen.css'
+import {isRouteErrorResponse, useRouteError} from "react-router-dom";
 
 export default function ErrorScreen() {
+
+    const error = useRouteError();
+    const errorMessage = isRouteErrorResponse(error) ?
+        error.statusText : error instanceof Error ? error.message :
+            "An unknown error occurred";
+
     return <div className="errorScreenContentContainer">
         <img src={ErrorIcon} alt="error-icon"/>
         <p>Oops! Something went wrong</p>
-        <p>reload the page ..</p>
+        <p>{errorMessage}</p>
     </div>
 }
